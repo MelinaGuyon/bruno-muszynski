88dc0a1eca25220af884fc4e1dc5b880a61257a9
Melina Guyon <melina.guyon@stinkstudios.com>
2019-04-16 16:35:31 +0200

    Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -17,7 +17,14 @@ class Header extends React.Component {
   }
 
   onRouteChange = () => {
-    // set active link
+    const spans = [...this.component.current.querySelectorAll('.navigation > span')];
+    const route = Router.getLastRoute();
+
+    spans.forEach((span) => {
+      span.classList.remove('active');
+      const spanroute = span.getAttribute('data-route');
+      if (spanroute === route) span.classList.add('active');
+    })
   }
 
   render () {
@@ -27,9 +34,9 @@ class Header extends React.Component {
           <img src='./images/logo.png'/>
         </div>
         <div className='navigation'>
-          <RouterComponent route="home">recherches</RouterComponent>
-          <a href="http://google.com" target='_blank' rel="noopener noreferrer">bibliographie</a>
-          <RouterComponent route="remerciements">remerciements</RouterComponent>
+          <span data-route='home'><RouterComponent route="home">recherches</RouterComponent></span>
+          <span><a href="http://google.com" target='_blank' rel="noopener noreferrer">bibliographie</a></span>
+          <span data-route='remerciements'><RouterComponent route="remerciements">remerciements</RouterComponent></span>
         </div>
       </header>
     );
