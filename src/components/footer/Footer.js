282cadd9f53147e13cd2f9311a1f4aa3157c0669
Melina Guyon <melina.guyon@stinkstudios.com>
2019-04-16 18:13:54 +0200

    Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Router from 'abstract/router/Router';
-import RouterComponent from 'abstract/router/RouterComponent';
+import i18n from 'abstract/i18n/i18n';
 
 require('./Footer.scss');
 
@@ -8,9 +7,12 @@ class Footer extends React.Component {
   component = React.createRef();
 
   render () {
+    const footer = i18n.localize('footer');
+
     return (
       <footer className='footer' ref={this.component}>
-
+        <span><img src="./images/social.svg" alt=""/><a href={footer.social.link} target='_blank' rel="noopener noreferrer">{footer.social.text}</a></span>
+        <span dangerouslySetInnerHTML={{ __html: footer.thanks }}></span>
       </footer>
     );
   }
